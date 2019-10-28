2f20f92a13d9e5b662ac392d4ef30741044050db
Aaron-石雪岩 <43869308+Aaron3963@users.noreply.github.com>
2019-10-28 08:55:03 +0800

    Create getTotalArea.java

---
+++ getTotalArea.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class getTotalArea{
+    public double getAllCircleArea(Circle [] list){
+        double area = 0;
+        for(int i = 0; i < list.length;i++){
+            area += list[i].getArea();
+        }
+        return area;
+    }
+
+    public double getAllRectangleArea(ArrayList<Rectangle> list){
+        double area = 0;
+        for(int i = 0; i < list.size(); i++){
+            area += list.get(i).getArea();
+        }
+        return area;
+    }
+
+    public double getAllTriangle(ArrayList<Triangle> list){
+        double area = 0;
+        for(int i = 0; i < list.size(); i++){
+            area += list.get(i).getArea();
+        }
+        return area;
+    }
+    public static void main(String[] args) {
+        Circle[] list = new Circle[3];
+        list[0] = new Circle(5.1);
+        list[1] = new Circle(114514);   
+
+        ArrayList<Rectangle> alistR = new ArrayList<Rectangle>();        
+        alistR.add(new Rectangle(14,5));
+        alistR.add(new Rectangle(2,10));
+
+        ArrayList<Triangle> alistT = new ArrayList<Triangle>();        
+        alistT.add(new Triangle(14,5));
+        alistT.add(new Triangle(2,10));
+    }
+}
